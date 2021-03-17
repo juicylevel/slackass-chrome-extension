f39b3cdcd77665fd71e7f28ff854f1bc2bed4e42
Yuri Rykov <rykov87yuri@gmail.com>
2021-03-17 10:41:26 +0300

    added project files

---
+++ bi-form.js
@@ -0,0 +1,22 @@
+const queryString = window.location.search;
+const searchParams = new URLSearchParams(queryString);
+
+const queryTypeIndex = searchParams.get('queryType');
+const taxId = searchParams.get('taxId');
+const bic = searchParams.get('bic');
+
+const queryTypeRadio = document.querySelector(`#unirad_${queryTypeIndex}`);
+const taxIdInput = document.querySelector('input[name=innPRS]');
+const bicInput = document.querySelector('input[name=bikPRS]');
+const submitButton = document.querySelector('#btnSearch');
+
+queryTypeRadio.click();
+taxIdInput.value = taxId;
+bicInput.value = bic;
+
+setTimeout(
+    () => {
+        submitButton.click();
+    },
+    1500
+);
